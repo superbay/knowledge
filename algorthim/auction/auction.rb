0b40d43e1ea7d16460b69617620f79c8d5d6be05
Ken Lee <klee589@bloomberg.net>
2013-09-12 15:09:06 -0400

    finished auction rspec

--- algorthim/auction/auction.rb
+++ algorthim/auction/auction.rb
@@ -1,7 +1,9 @@
 module Auction
   class Item
-    attr_accessor :name, :reserved_price, :status, :sold_price, :bid_list, :current_win
+    attr_accessor :name, :reserved_price, :status, :sold_price, :bid_list, :current_bid
     def initialize(name, reserved_price)
+      self.current_bid = nil 
+      self.bid_list = []
       self.name = name
       self.reserved_price = reserved_price
       self.status = 'ready'
@@ -11,13 +13,13 @@ module Auction
   class Auction
     attr_accessor :ready_items, :sold_items
     def initialize(items)
-      return 'include sold item' if items.status == 'sold'
+      return 'include sold item' if items.any?{|item| item.status == 'sold'}
       self.ready_items = items
     end
 
     def start_bid(item, bid)
-      return 'fail bid' if !self.items.include?(item) || bid.price <= self.current_win.price
-      item.current_win = bid
+      return 'fail bid' if item.status == 'sold' || !self.ready_items.include?(item) || (item.current_bid && bid.price <= item.current_bid.price)
+      item.current_bid = bid
       item.bid_list << bid
       if bid.price > item.reserved_price
         item.status = 'sold'
--- algorthim/auction/auction_spec.rb
+++ algorthim/auction/auction_spec.rb
@@ -1,12 +1,44 @@
 load 'auction.rb'
-#include Auction
 describe Auction do
   context 'new item' do
-    it 'should create new item and add to autcion' do
+    it 'should create new item and add to auction' do
       item = Auction::Item.new('moon', 99.99)
       item.status.should eq 'ready' 
       auction = Auction::Auction.new([item])
       auction.ready_items.should include item
     end
+  end
+
+  context 'bid auction' do
+    before :each do 
+      @moon = Auction::Item.new('moon', 99.99)
+      sun = Auction::Item.new('sun', 999.99)
+      earth = Auction::Item.new('earth', 666.66)
+      @auction = Auction::Auction.new([@moon, sun])
+      bid1 = Auction::Bid.new('Ken', 66.66)
+      @auction.start_bid(@moon, bid1)
+    end
+
+    it 'should has to have a price higher than the current highest bid' do
+      bid2 = Auction::Bid.new('Ken', 33.33)
+      @auction.start_bid(@moon, bid2).should eq 'fail bid'
+      bid3 = Auction::Bid.new('Ken', 77.33)
+      expect{@auction.start_bid(@moon, bid3)}.to change{@moon.bid_list.count}.by(1)
+      expect(@moon.current_bid.price).to eq 77.33
+    end
+   
+    it 'should close the auction for the item while bid price is highest' do 
+      bid4 = Auction::Bid.new('Ken', 100)
+      expect{@auction.start_bid(@moon, bid4)}.to change{@moon.bid_list.count}.by(1)
+      expect(@moon.status).to eq 'sold'
+    end
+
+    it 'The item sold should be no longer available for future auctions' do
+      bid4 = Auction::Bid.new('Ken', 100)
+      expect{@auction.start_bid(@moon, bid4)}.to change{@moon.bid_list.count}.by(1)
+      bid5 = Auction::Bid.new('Ken', 200)
+      @auction.start_bid(@moon, bid5).should eq 'fail bid' 
+    end
+
   end
 end
