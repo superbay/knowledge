17eea6461657ff4807dff4510a39f8867705eb58
Ken Lee <klee589@bloomberg.net>
2013-09-12 15:14:10 -0400

    finished by adding another method

--- algorthim/consecutive_number/consecutive.rb
+++ algorthim/consecutive_number/consecutive.rb
@@ -52,3 +52,21 @@ def find_consecutive_runs3(list =  [1, 2, 3, 4, 10, 9, 8, 9, 10, 11, 7] )
   rs
 end
 
+def find_consecutive_runs4(list =  [1, 2, 3, 4, 10, 9, 8, 9, 10, 11, 7] )
+  rs = []
+  pre_status = 'false'
+  current_status = 'false'
+  list.each_with_index do |item, i|
+    next if i == 0
+    current_status = if (list[i - 1] + 1) == list[i]
+      'asc' 
+    elsif (list[i - 1] - 1) == list[i]
+      'desc' 
+    else
+      'false'
+    end
+    rs << (i - 2) if pre_status != 'false' && pre_status == current_status
+    pre_status = current_status
+  end
+  rs
+end
