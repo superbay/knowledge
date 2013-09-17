8775a4f05626cdbc59cb3b8b26d20f932a5c9068
Ken Lu <ken@seeice.com>
2013-09-17 07:56:03 -0400

    updated with more requirement

--- algorthim/consecutive_number/consecutive.rb
+++ algorthim/consecutive_number/consecutive.rb
@@ -1,13 +1,23 @@
-def find_consecutive_runs(list =  [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7] )
+def find_consecutive_runs(list =  [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7], n = 3 )
   rs = []
   list.each_with_index do |x, i|
-    rs << i if list[i + 2] && is_con?(x, list[i+1], list[i + 2])
+    rs << i if list[i + n] && is_con?(list, i, n)
   end
   rs
 end
 
-def is_con?(x, y, z)
-  ((x == (y + 1) && y = (z + 1) ) || ( x == (y - 1 ) && y == (z - 1) ) ) ? true : false
+def is_con?(list, start_index, n)
+  asc = start_index.upto(n).each do |i|
+    return false unless list[i] + 1 == list[i + 1]
+  end
+
+  desc = start_index.upto(n).each do |i|
+    return false unless list[i] - 1 == list[i + 1]
+  end
+
+  #((x == (y + 1) && y = (z + 1) ) || ( x == (y - 1 ) && y == (z - 1) ) ) ? true : false
+  asc || desc
+
 end
 
 
@@ -29,30 +39,7 @@ def find_consecutive_runs2(list =  [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7] )
   rs
 end
 
-
 def find_consecutive_runs3(list =  [1, 2, 3, 4, 10, 9, 8, 9, 10, 11, 7] )
-  rs = []
-  counter = 0
-  pointer = 0
-  0.upto(list.length).each do |i|
-    next if i == 0 || i < pointer
-    pointer = i.upto(list.length).each do |j|
-      if counter >= 0 && list[j] == list[j - 1] + 1  
-        counter += 1 
-        rs << j - 2 if counter >= 2
-      elsif counter <= 0 && list[j]== list[j - 1] - 1  
-        counter -= 1
-        rs << j - 2 if counter <= -2
-      else
-        counter = 0
-        break j
-      end
-    end
-  end
-  rs
-end
-
-def find_consecutive_runs4(list =  [1, 2, 3, 4, 10, 9, 8, 9, 10, 11, 7] )
   rs = []
   pre_status = 'false'
   current_status = 'false'
